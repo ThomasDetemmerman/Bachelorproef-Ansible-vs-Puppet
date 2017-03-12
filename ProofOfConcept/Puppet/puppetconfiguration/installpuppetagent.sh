0737a3eeeed3f587b4f673133c58e19649674372
Thomas <detemmerman.thomas@hotmail.com>
2017-03-12 13:29:19 +0100

    verwijderd dubbel script en comprimeren script

--- ProofOfConcept/Puppet/puppetconfiguration/installpuppetagent.sh
+++ ProofOfConcept/Puppet/puppetconfiguration/installpuppetagent.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 yum install puppet -y
-
 yes | cp /vagrant/puppetconfiguration/hosts /etc/hosts
-
 yes | cp /vagrant/puppetconfiguration/puppet-agent.conf /etc/puppet/puppet.conf
-
 systemctl start puppet
-
 puppet agent -t
