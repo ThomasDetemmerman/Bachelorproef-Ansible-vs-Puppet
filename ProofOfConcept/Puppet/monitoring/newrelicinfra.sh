684b38178983b701358acca9133aa23aad68d79b
Thomas <detemmerman.thomas@hotmail.com>
2017-04-23 14:43:52 +0200

    solved design issue + Puppet network dataset cleanup (laden van pagina veroorzaakte incorrecte resultaten)

--- ProofOfConcept/Puppet/monitoring/newrelicinfra.sh
+++ ProofOfConcept/Puppet/monitoring/newrelicinfra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "license_key: 9ed50a0ac1490485d0e0b6a3fb2d17babb922c0d" | sudo tee -a /etc/newrelic-infra.yml
+echo "license_key: 71b9436de77bf97dae3d2b9a77230843ec1111f2" | sudo tee -a /etc/newrelic-infra.yml
 
 printf "[newrelic-infra]\nname=New Relic Infrastructure\nbaseurl=http://download.newrelic.com/infrastructure_agent/linux/yum/el/7/x86_64\nenable=1\ngpgcheck=0" | sudo tee -a /etc/yum.repos.d/newrelic-infra.repo
 
@@ -8,4 +8,3 @@ yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
 
 sudo yum install newrelic-infra -y
 systemctl start newrelic-infra
-
